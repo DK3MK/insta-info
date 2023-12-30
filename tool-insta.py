27a9329a660abf8011385a27e41e3b75be7fea3c
DK3MK <95143027+DK3MK@users.noreply.github.com>
2023-12-31 02:12:25 +0400

    Fix

--- tool-insta.py
+++ tool-insta.py
@@ -6,10 +6,14 @@ try:
 except:
     print('Done')
 
-import requests
-from user_agent import generate_user_agent
+# this tool information account on instagram . . .
+# بسم الله رب البدايات نبدأ
+import requests, json
 from colorama import Fore,Style
+from user_agent import generate_user_agent
+os.system('cls' if os.name == 'nt' else 'clear')
 
+# colors :
 b0 = Style.RESET_ALL
 b1 = Fore.YELLOW
 b4 = Fore.LIGHTCYAN_EX
@@ -18,7 +22,6 @@ b6 = Fore.RED
 b7 = Fore.GREEN
 b8 = Fore.MAGENTA
 b9 = Fore.BLUE
-os.system('cls' if os.name == 'nt' else 'clear')
 logo = f"""{b1}
    /$$                         /$$
   | $$                        | $$
@@ -35,13 +38,13 @@ logo = f"""{b1}
         | $$| $$  \ $$|  $$$$$$   | $$      /$$$$$$$
         | $$| $$  | $$ \____  $$  | $$ /$$ /$$__  $$
         | $$| $$  | $$ /$$$$$$$/  |  $$$$/|  $$$$$$$
-        |__/|__/  |__/|_______/    \___/   \_______/ {b8}version 3
+        |__/|__/  |__/|_______/    \___/   \_______/ {b8}version 4
 {b1}                    
 {b6}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 {b6}┃ {b5}Coded By {b8}DARK MAN 747{b5} || Follow me for more.    {b6}┃
 {b6}┃           {b0}━{b8}━{b0}━{b8}━{b0}━{b8}━{b0}━{b8}━{b0}━{b8}━{b0}━{b8}━{b0}━{b8}━{b0}━{b8}━{b0}━{b8}━{b0}━{b8}━{b0}━{b8}━{b0}━{b8}━{b0}━{b8}━            {b6}┃
 {b6}┃   {b0}[{b1}+{b0}] {b7}InstaGram: {b4}aboud_coding                   {b6}┃
-{b6}┃   {b0}[{b1}+{b0}] {b7}GitHub: {b4}Dark-Man747                       {b6}┃
+{b6}┃   {b0}[{b1}+{b0}] {b7}GitHub: {b4}DK3MK                             {b6}┃
 {b6}┃   {b0}[{b1}+{b0}] {b7}TikTok: {b4}kmhp                              {b6}┃
 {b6}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
     """
@@ -49,7 +52,6 @@ print(logo)
 
 target = input(f'{b0}[{b1}?{b0}] {b9}Enter username: {b6}')
 
-url = f"https://www.instagram.com/{target}/?__a=1&__d=dis"
 head = {
     'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-encoding':'gzip,deflate,br',
@@ -58,17 +60,24 @@ head = {
     'user-agent': generate_user_agent(),#'Instagram 9.4.0 Android (30/11; 480dpi; 1080x2158; OPPO; CPH2069; OP4C7BL1; qcom; en_US; 276028020)',#generate_user_agent(),
 
     }
-information = requests.get(url,headers=head).json()
 
-id = (information['graphql']['user']['id'])
-name = (information['graphql']['user']['full_name'])
-Bio = (information['graphql']['user']['biography'])
-Followers = (information['graphql']['user']['edge_followed_by']['count'])
-Following = (information['graphql']['user']['edge_follow']['count'])
-url = (information['graphql']['user']['external_url'])
-private = (information['graphql']['user']['is_private'])
-business = (information['graphql']['user']['is_business_account'])
-posts = str(information['graphql']['user']["edge_owner_to_timeline_media"]["count"])
+url = f'https://api-darkman-ffaction747.replit.app/insta?query={target}'
+
+info = requests.get(url).json()
+information = info['result']
+
+
+id = (information['id'])
+name = (information['full name'])
+Bio = (information['bio'])
+Followers = (information['follow'])
+Following = (information['following'])
+url = (information['bio link'])
+private = (information['private'])
+business = (information['business account'])
+professional = (information['professional account'])
+posts = str(information['posts'])
+url_pic = (information['profile pic'])
 re = requests.get(f"https://o7aa.pythonanywhere.com/?id={id}",headers=head)
 ree = re.json()
 dat = ree['date']
@@ -84,4 +93,6 @@ print(f"""
 {b0}[{b1}+{b0}] {b4}Bio link {b1}:{b0} {url}
 {b0}[{b1}+{b0}] {b4}Private {b1}:{b0} {private}
 {b0}[{b1}+{b0}] {b4}Business Account {b5}:{b0} {business}
+{b0}[{b1}+{b0}] {b4}professional account {b5}:{b0} {professional}
+{b0}[{b1}+{b0}] {b4}pic url :{b0} {url_pic}
 """)
